686a4dd0a8fba07da4e19689a35e426bfe87daa9
nasu-1103 <ayachooo113@gmail.com>
2024-10-22 13:37:29 +0900

    コメントの修正と画面の編集

--- resources/views/watch_lists/edit.blade.php
+++ resources/views/watch_lists/edit.blade.php
@@ -11,9 +11,9 @@
                 <div class="p-6 text-gray-900">
                     <div class="w-full">
 
-                        {{-- エラーメッセージを表示 --}}
+                        {{-- バリデーションエラーがあれば、エラーメッセージを表示 --}}
                         @if ($errors->any())
-                            <ul>
+                            <ul class="ml-4">
                                 @foreach ($errors->all() as $error)
                                     <li>{{ $error }}</li>
                                 @endforeach
@@ -23,20 +23,27 @@
                         <form action="{{ route('watch_list.update', $watch_list) }}" method="POST" class="space-y-4">
                             @csrf
                             @method('PATCH')
-                            <label for="anime"
-                                class="block mb-2 text-lg font-medium text-gray-700 dark:text-white">アニメを選択してください。</label>
-                            <div class="relative">
-                                <select name="anime_id" id="anime"
-                                    class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-base">
-                                    @foreach ($animes as $anime)
-                                        <option value="{{ $anime->id }}" @selected($anime->id == $watch_list->anime_id)>
-                                            {{ $anime->title . ' ： ' . $anime->sub_title }}
-                                        </option>
-                                    @endforeach
-                                </select>
+                            <div class="mb-3 mt-2">
+                                <label for="anime"
+                                    class="ml-4 block text-lg font-medium text-gray-700 dark:text-white">アニメを選択してください。</label>
+                                <div class="flex">
+                                    <select name="anime_id" id="anime"
+                                        class="ml-4 mt-2 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-base">
+                                        {{-- 全てのアニメのリストを表示 --}}
+                                        @foreach ($animes as $anime)
+                                            <option value="{{ $anime->id }}" @selected($anime->id == $watch_list->anime_id)>
+                                                {{-- アニメのタイトルとサブタイトルを表示 --}}
+                                                {{ $anime->title . ' ： ' . $anime->sub_title }}
+                                            </option>
+                                        @endforeach
+                                    </select>
+                                </div>
                             </div>
-                            <a href="{{ route('watch_list.index') }}" class="btn btn-ghost">&lt; 戻る</a>
-                            <button type="submit" class="btn btn-outline btn-info">更新</button>
+
+                            <div class="mb-3 ml-4">
+                                <a href="{{ route('watch_list.index') }}" class="btn btn-ghost">&lt; 戻る</a>
+                                <button type="submit" class="btn btn-outline btn-info ml-2">更新</button>
+                            </div>
                         </form>
                     </div>
                 </div>
