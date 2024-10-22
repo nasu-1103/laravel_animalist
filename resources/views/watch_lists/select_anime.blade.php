2065c512dc22cbc600c2b071234dc9f05c33be9c
nasu-1103 <ayachooo113@gmail.com>
2024-10-22 13:35:54 +0900

    不要コードの削除と画面の修正

--- resources/views/watch_lists/select_anime.blade.php
+++ resources/views/watch_lists/select_anime.blade.php
@@ -11,35 +11,34 @@
                 <div class="p-6 text-gray-900">
                     <div class="w-full">
 
-                        {{-- エラーメッセージを表示 --}}
-                        @if ($errors->any())
-                            <ul>
-                                @foreach ($errors->all() as $error)
-                                    <li>{{ $error }}</li>
-                                @endforeach
-                            </ul>
-                        @endif
-
-                        <a href="{{ route('anime_groups.setting') }}" class="underline text-blue-500 mt-1">非表示設定</a>
+                        <a href="{{ route('anime_groups.setting') }}" class="ml-4 underline text-blue-500">非表示設定</a>
 
                         <form action="{{ route('watch_list.create') }}" method="GET" class="space-y-4">
                             @csrf
-                            <label for="anime"
-                                class="block mb-2 text-lg font-medium text-gray-700 dark:text-white">アニメを選択してください。</label>
-                            <div class="relative">
-                                <select name="animeGroupId" id="anime"
-                                    class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-base">
-                                    @foreach ($animeGroups as $animeGroup)
-                                        @unless (in_array($animeGroup->id, $userHiddenLists))
-                                            <option value="{{ $animeGroup->id }}" @selected($animeGroup->id == old('animeGroupId'))>
-                                                {{ $animeGroup->name }}
-                                            </option>
-                                        @endunless
-                                    @endforeach
-                                </select>
+                            <div class="mb-3 mt-2">
+                                <label for="anime"
+                                    class="ml-4 block text-lg font-medium text-gray-700 dark:text-white">アニメを選択してください。</label>
+                                <div class="flex">
+                                    <select name="animeGroupId" id="anime"
+                                        class="ml-4 mt-2 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-base">
+                                        {{-- 各アニメグループをチェック --}}
+                                        @foreach ($animeGroups as $animeGroup)
+                                            {{-- 非表示リストにない場合、アニメグループをリストに追加 --}}
+                                            @unless (in_array($animeGroup->id, $userHiddenLists))
+                                                <option value="{{ $animeGroup->id }}" @selected($animeGroup->id == old('animeGroupId'))>
+                                                    {{-- アニメのタイトルを表示 --}}
+                                                    {{ $animeGroup->name }}
+                                                </option>
+                                            @endunless
+                                        @endforeach
+                                    </select>
+                                </div>
                             </div>
-                            <a href="{{ route('watch_list.index') }}" class="btn btn-ghost">&lt; 戻る</a>
-                            <button type="submit" class="btn btn-outline btn-info">選択</button>
+
+                            <div class="mb-3 ml-4">
+                                <a href="{{ route('watch_list.index') }}" class="btn btn-ghost">&lt; 戻る</a>
+                                <button type="submit" class="btn btn-outline btn-info ml-2">選択</button>
+                            </div>
                         </form>
                     </div>
                 </div>
