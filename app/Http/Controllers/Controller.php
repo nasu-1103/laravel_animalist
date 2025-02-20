e2dcfe9f21f5dc3788d81e589c13e01ea96d16cc
nasu-1103 <ayachooo113@gmail.com>
2025-02-20 10:32:14 +0900

    AuthorizesRequests ValidatesRequestsの削除

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,16 +2,12 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
-use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Gate;
 
 class Controller extends BaseController
 {
-    use AuthorizesRequests, ValidatesRequests;
-
     // 管理者以外のアクセスを制限
     protected function authorizeAdmin()
     {
